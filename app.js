faa26850541023dcc8bb20c236dd05ac9dffe755
Alex Merced <alex@alexmerced.dev>
2023-02-17 17:22:31 -0500

    update

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 import { NoteManager, NoteRenderer } from "./notes.js";
 
-const mgr = new NoteManager()
-const render = new NoteRenderer(document.querySelector("main"), mgr)
-const form = document.querySelector("form")
-const search = document.querySelector("input[name='search'")
-const exportButton = document.querySelector("#export")
+const mgr = new NoteManager();
+const render = new NoteRenderer(document.querySelector("main"), mgr);
+const form = document.querySelector("form");
+const search = document.querySelector("input[name='search'");
+const exportButton = document.querySelector("#export");
 
 // initial render
-render.renderNotes()
+render.renderNotes();
 
 // add note
-form.addEventListener("submit", function(event){
-    event.preventDefault()
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    const formData = new FormData(this)
+  const formData = new FormData(this);
 
-    const newNote = {
-        note: formData.get("note"),
-        tags: formData.get("tags")
-    }
+  const newNote = {
+    note: formData.get("note"),
+    tags: formData.get("tags"),
+  };
 
-    mgr.addNote(newNote)
-    render.renderNotes()
-})
+  mgr.addNote(newNote);
+  render.renderNotes();
+});
 
 search.addEventListener("input", (event) => {
-    render.renderNotes(mgr.searchNotes(event.target.value))
-})
+  render.renderNotes(mgr.searchNotes(event.target.value));
+});
 
 exportButton.addEventListener("click", (event) => {
-    mgr.exportNotes()
-})
+  mgr.exportNotes();
+});
